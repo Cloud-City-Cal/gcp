4ab0f35cac126fc98ec22e5631a12f2a97412ada
Qingyang Hu <12138874+hqy2000@users.noreply.github.com>
2023-11-17 19:17:09 -0800

    Switch to Pub/Sub

--- cloud_function/open_weather/open_weather.py
+++ cloud_function/open_weather/open_weather.py
@@ -1,133 +1,34 @@
-import requests, json
-from datetime import datetime
-from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
+import os
+import sys
 
-project_id = "cloud-city-cal"
-dataset_id = "open_weather"
-API_KEY = "YOUR_KEY"
+import functions_framework
+import requests
+from cloudevents.http import CloudEvent
+from google.cloud.pubsub import PublisherClient
+from google.protobuf.json_format import ParseDict
 
+from weather_pb2 import Weather
 
-def insert_into_bigquery(request):
-    # Create a reference to the dataset and table
-    api_key = API_KEY
-    city_names = ["San Mateo", "Berkeley", "Alameda", "Oakland", "Fruitvale"]
-    client = bigquery.Client(project=project_id)
-    dataset_ref = client.dataset(dataset_id)
-    table_id_weather = "weather"
-    table_ref_weather = dataset_ref.table(table_id_weather)
-    table_weather = client.get_table(table_ref_weather)
-    table_id_geo = "geo"
-    table_ref_geo = dataset_ref.table(table_id_geo)
-    table_geo = client.get_table(table_ref_geo)
+API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
 
-    for city_name in city_names:
-        query_job = client.query(
-            f"""
-                SELECT lat, lon
-                FROM `{project_id}.{dataset_id}.{table_id_geo}`
-                WHERE name = '{city_name}'
-            """
-        )
-        result = query_job.result()
-        lat, lon = next(result)
+'''
+Environment Variables:
+API_KEY: API Key for openweathermap.org
+PROJECT_ID: Current project ID
+TOPIC_ID: Topic ID of the published message
+'''
 
-        unit = "metric"
-        base_url = "https://api.openweathermap.org/data/2.5/weather"
+@functions_framework.cloud_event
+def collect_weather_data(cloud_event: CloudEvent):
+    response = requests.get(API_ENDPOINT, params={
+        "lon": cloud_event.data['message']['lon'],
+        "lat": cloud_event.data['message']['lat'],
+        "appid": os.environ['API_KEY'],
+        "units": "metric",
+    })
+    response.raise_for_status()
+    proto = ParseDict(response.json(), Weather())
 
-        params = {
-            "lon": lon,
-            "lat": lat,
-            "appid": api_key,
-            "units": unit,
-        }
-
-        try:
-            response = requests.get(base_url, params=params)
-            response.raise_for_status()
-            weather_data = response.json()
-
-            """
-            Sample Output
-            {
-                "coord": {
-                    "lon": -122.16,
-                    "lat": 37.71
-                },
-                "weather": [
-                    {
-                    "id": 801,
-                    "main": "Clouds",
-                    "description": "few clouds",
-                    "icon": "02n"
-                    }
-                ],
-                "base": "stations",
-                "main": {
-                    "temp": 13.81,
-                    "feels_like": 13.34,
-                    "temp_min": 11.42,
-                    "temp_max": 16.15,
-                    "pressure": 1013,
-                    "humidity": 80
-                },
-                "visibility": 10000,
-                "wind": {
-                    "speed": 3.6,
-                    "deg": 320
-                },
-                "clouds": {
-                    "all": 20
-                },
-                "dt": 1699930356,
-                "sys": {
-                    "type": 2,
-                    "id": 2004596,
-                    "country": "US",
-                    "sunrise": 1699886792,
-                    "sunset": 1699923568
-                },
-                "timezone": -28800,
-                "id": 5392263,
-                "name": "San Leandro",
-                "cod": 200
-            }
-            """
-
-            if "main" not in weather_data:
-                continue
-            if "coord" in weather_data:
-                for ele in weather_data["coord"]:
-                    weather_data[ele] = weather_data["coord"][ele]
-                del weather_data["coord"]
-            if "main" in weather_data:
-                for ele in weather_data["main"]:
-                    weather_data[ele] = weather_data["main"][ele]
-                del weather_data["main"]
-            weather_data["weather"] = str(weather_data["weather"])
-            if "wind" in weather_data:
-                weather_data["wind_speed"] = weather_data["wind"]["speed"]
-                weather_data["wind_deg"] = weather_data["wind"]["deg"]
-                del weather_data["wind"]
-            if "clouds" in weather_data:
-                weather_data["clouds"] = weather_data["clouds"]["all"]
-            if "sys" in weather_data:
-                weather_data["sunrise"] = weather_data["sys"]["sunrise"]
-                weather_data["sunset"] = weather_data["sys"]["sunset"]
-                del weather_data["sys"]
-            if "rain" in weather_data:
-                for ele in weather_data["rain"]:
-                    weather_data["rain_" + ele] = weather_data["rain"][ele]
-                del weather_data["rain"]
-            if "snow" in weather_data:
-                for ele in weather_data["snow"]:
-                    weather_data["snow_" + ele] = weather_data["snow"][ele]
-                del weather_data["snow"]
-            if "cod" in weather_data:
-                del weather_data["cod"]
-            errors = client.insert_rows(table_weather, [weather_data])
-            if errors:
-                return f"Error inserting weather data into BigQuery: {errors}"
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-    return "ok"
+    publisher_client = PublisherClient()
+    topic_path = publisher_client.topic_path(os.environ['PROJECT_ID'], os.environ['TOPIC_ID'])
+    publisher_client.publish(topic_path, proto.SerializeToString())
