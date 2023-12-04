68b4e6ef7be37d7a0205c6f404ed9db4dd3f9df6
Qingyang Hu <me@hqy.moe>
2023-12-03 18:52:56 -0800

    PeMS dedup

--- cloud_function/PeMS/PeMS.py
+++ cloud_function/PeMS/PeMS.py
@@ -1,14 +1,16 @@
 import os
 from io import StringIO
+from typing import List
 
 import requests
 import csv
 import datetime
+import json
 from cloudevents.http import CloudEvent
-from google.pubsub import PublisherClient
+from google.cloud.pubsub import PublisherClient
+from google.cloud import bigquery
 
 from pems_pb2 import PeMS
-from google.protobuf import text_format
 
 BASE_URL = "https://pems.dot.ca.gov"
 TIME_RANGE = 60 * 60  # 60 mins interval
@@ -24,9 +26,10 @@ TOPIC_ID: Topic ID of the published message
 '''
 
 
-def insert_into_bigquery(cloud_event: CloudEvent):
+def get_pems_data(cloud_event: CloudEvent):
     # Get the station id from big query or request
-    stations = [401643]
+    with open('station_ids.json') as f:
+        stations = json.load(f)
 
     session = requests.Session()
     # Log in
@@ -41,12 +44,14 @@ def insert_into_bigquery(cloud_event: CloudEvent):
 
     # PeMS is timezone unaware, so we treat Pacific Time as UTC.
     timestamp_now = int(datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp())
+
     publisher_client = PublisherClient()
     topic_path = publisher_client.topic_path(os.environ['PROJECT_ID'], os.environ['TOPIC_ID'])
 
 
     # Crawl Data
     for station_id in stations:
+        print(f"Fetching data for station_id: {station_id}.")
         params = {
             "report_form": 1,
             "dnode": "VDS",
@@ -84,8 +89,24 @@ def insert_into_bigquery(cloud_event: CloudEvent):
         reader = csv.DictReader(StringIO(res.text), delimiter="\t", quoting=csv.QUOTE_NONE)
         for row in reader:
             lanes_len = int(row["# Lane Points"])
-            data = PeMS(station_id=station_id, time=row["5 Minutes"], lanes=[])
+            data = PeMS(station_id=station_id, time=row["5 Minutes"], observed=float(row["% Observed"]), lanes=[])
             for i in range(1, lanes_len + 1):
                 data.lanes.append(PeMS.Lane(speed=float(row[f"Lane {i} Speed (mph)"]), flow=float(row[f"Lane {i} Flow (Veh/5 Minutes)"])))
             publisher_client.publish(topic_path, data.SerializeToString())
 
+
+def get_missing_record_ids() -> List:
+    client = bigquery.Client(project=os.environ['PROJECT_ID'])
+
+    query_job = client.query(
+        f"""
+        SELECT station_id, MAX(`time`) as lastest_time
+        FROM {os.environ['PROJECT_ID']}.{os.environ['DATASET_ID']}.{os.environ['TABLE_ID']}
+        GROUP BY station_id
+        """)
+
+    return query_job.result()
+
+
+if __name__ == '__main__':
+    get_pems_data(None)
