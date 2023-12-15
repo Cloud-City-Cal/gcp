09785958c9333fd8b984473916cf3e2061c721db
jeff-901 <b07901052@ntu.edu.tw>
2023-12-15 10:12:44 -0800

    update aggregation function

--- preprocess/PeMS/PeMS.py
+++ preprocess/PeMS/PeMS.py
@@ -1,8 +1,6 @@
-import json, os, requests
-from collections import defaultdict
+import json, os
 from google.cloud import bigquery
 import functions_framework
-from datetime import datetime
 
 """
 Environment Variables:
@@ -12,6 +10,7 @@ TABLE_ID: Table ID to check for existing records
 TOPIC_ID: Topic ID of the published message
 """
 
+
 @functions_framework.http
 def preprocess(request):
     client = bigquery.Client("cloud-city-cal")
@@ -19,21 +18,24 @@ def preprocess(request):
     # TODO read segment_definition from storage
     with open("segment_definition.json", "r") as f:
         segment_definition = json.load(f)
-    
+
     # TODO read timestamp_s from cloud event
     timestamp_s = "2023-11-18 19:00:00"
-
     for segment in segment_definition:
         query_job = client.query(
             f"""
-            SELECT agg_speed
+            SELECT station_id, agg_speed
             FROM {os.environ['PROJECT_ID']}.{os.environ['DATASET_ID']}.{os.environ['TABLE_ID']}
-            WHERE station_id in UNNEST({segment["station_id"]}) AND
+            WHERE station_id in UNNEST({list(segment["station_ids"].keys())}) AND
             time == DATETIME({json.dumps(timestamp_s)})
-            """)
+            """
+        )
         segment_id = segment["id"]
         result = query_job.result()
-        agg_speed = sum(result) / len(result)
+
+        agg_speed = (segment["end_postmile"] - segment["start_postmile"]) / sum(
+            map(lambda x: segment["station_ids"][x[0]] / x[1], result)
+        )
         # TODO Insert into pubsub bigquery
         # segment_id, timestamp_s, agg_speed
     return "ok"
